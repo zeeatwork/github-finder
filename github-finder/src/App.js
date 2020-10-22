c23550a177647184586ad4cac9b399363f5bd7a3
Zenzi Ali <zeeathome935@gmail.com>
2020-10-22 17:21:54 -0400

    Created Navbar and Users page components

---
+++ github-finder/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/layouts/Navbar.js"
+import Users from "./components/users/Users";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar title="Github Finder" icon="fab fa-github" />
+        <div className="container">
+          <Users />
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
